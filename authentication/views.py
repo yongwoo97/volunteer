117959ce8b043f7295c39707b9a1edf6a92462f3
yongwoo97 <woo22661@naver.com>
2021-12-23 23:14:03 +0900

    게시판 기능 구현 완료, 로그인시 토큰 발행완료

--- authentication/views.py
+++ authentication/views.py
@@ -2,10 +2,9 @@
 from rest_framework.authtoken.models import Token
 from rest_framework import permissions
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.response import Response
 from .serializers import UserSerializer
 from django.http import JsonResponse
-
+from django.contrib.auth import authenticate
 
 @api_view(['POST', ])
 @permission_classes((permissions.AllowAny,))
@@ -28,3 +27,21 @@ def registration_view(request):
         else:
             data = serializer.errors
         return JsonResponse(data)
+
+
+#로그인 뷰, 아이디 패스워드 체크하고 맞으면 토큰 발급
+@api_view(['POST'])
+@permission_classes((permissions.AllowAny, ))
+def login_view(request):
+    if request.method == 'POST':
+        email = request.data.get('email')
+        print(email)
+        password = request.data.get('password')
+
+        user = authenticate(username = email, password = password)
+        if not user:
+            return JsonResponse({'error': 'invalid user'})
+
+        token, _ = Token.objects.get_or_create(user=user)
+        return JsonResponse({'token': token.key})
+#프로필 get view 자기 프로필이면 반환
--- config/urls.py
+++ config/urls.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
-from authentication.views import registration_view
-from rest_framework.authtoken.views import obtain_auth_token
+from authentication.views import registration_view, login_view
+
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/register/', registration_view, name='register_user'),
+    path('api/login/', login_view, name='login_user'),
     path('api/', include('board.urls')),
     # path('api/auth/login/', obtain_auth_token, name='login'),
     #path('api/auth/login/', TokenAuthenticationView.as_view(), name='login'),
